d52b6320586af0155f0cb4a1ec177f550ad41412
Deep Samanta <deepksam@Deeps-MacBook-Air.local>
2026-02-04 02:40:52 +0530

    f

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ import os
 
 
 # ===== COINDCX API =====
-COINDCX_KEY = os.getenv("COINDCXKEY")
+COINDCX_KEY = os.getenv("COINDCX_KEY")
 COINDCX_SECRET = os.getenv("COINDCX_SECRET")
 
 # ==============================
